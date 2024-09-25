8ad795babce5aa5e456ef4cb1b2780638546f14a
David Dudas <david.dudas@outlook.com>
2024-09-25 20:12:35 +0200

    fix a typo

--- bme_ros2_tutorials_py/scripts/hello_world.py
+++ bme_ros2_tutorials_py/scripts/hello_world.py
@@ -5,7 +5,7 @@ import rclpy # Import ROS2 python interface
 def main(args=None):
     rclpy.init(args=args)                          # Initialize the ROS2 python interface
     node = rclpy.create_node('python_hello_world') # Node constructor, give it a name
-    node.get_logger().info("Hello, ROS2!")          # Use the ROS2 node's built in logger
+    node.get_logger().info("Hello, ROS2!")         # Use the ROS2 node's built in logger
     node.destroy_node()                            # Node destructor
     rclpy.shutdown()                               # Shut the ROS2 python interface down
 
