52fd487e6d1ee226b584a69c7d82e89f72b8b189
David Dudas <david.dudas@outlook.com>
2024-09-29 16:14:04 +0200

    add a new launch file with parameters

---
+++ bme_ros2_tutorials_bringup/launch/publisher_param.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    cpp_publisher_node = Node(
+        package="bme_ros2_tutorials_py",
+        executable="py_publisher_with_param",
+        name="my_publisher",
+        parameters=[{"published_text": "Parameter_from_launch"}]
+    )
+
+
+    ld.add_action(cpp_publisher_node)
+    return ld
